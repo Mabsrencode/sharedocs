ba734663f1c0968f6192af7f40d7b8f19b1cadf1
Reniel George Mababa <mabsrencode@gmail.com>
2025-01-20 14:56:08 +0800

    Client Suspense.

--- app/provider/Provider.tsx
+++ app/provider/Provider.tsx
@@ -1,18 +1,15 @@
 import React from "react";
 import {
   LiveblocksProvider,
-  RoomProvider,
   ClientSideSuspense,
 } from "@liveblocks/react/suspense";
 import Loader from "@/components/common/Loader/Loader";
 const Provider = ({ children }: { children: React.ReactNode }) => {
   return (
     <LiveblocksProvider authEndpoint={"/api/liveblocks-auth"}>
-      <RoomProvider id="my-room">
-        <ClientSideSuspense fallback={<Loader bigger />}>
-          {children}
-        </ClientSideSuspense>
-      </RoomProvider>
+      <ClientSideSuspense fallback={<Loader bigger />}>
+        {children}
+      </ClientSideSuspense>
     </LiveblocksProvider>
   );
 };
