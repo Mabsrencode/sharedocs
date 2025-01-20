f5409189d114ca6383f709f7f6fa0f27d9570643
Reniel George Mababa <mabsrencode@gmail.com>
2025-01-20 12:00:06 +0800

    feat: ClerkProvider, Clerk Theme.

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,9 +1,15 @@
+import Header from "@/components/common/Header/Header";
 import { Editor } from "@/components/editor/Editor";
 import React from "react";
 
 const page = () => {
   return (
     <div>
+      <Header>
+        <div className="flex w-fit items-center justify-center gap-2">
+          <p className="document-title">This is a fake document title.</p>
+        </div>
+      </Header>
       <Editor />
     </div>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import "./globals.css";
 
 import { cn } from "@/lib/utils";
 import { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 const fontSans = FontSans({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -31,15 +33,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+        variables: { colorPrimary: "#3371FF", fontSize: "16px" },
+      }}
+    >
+      <html lang="en">
+        <body
+          className={cn(
+            "min-h-screen bg-background font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
