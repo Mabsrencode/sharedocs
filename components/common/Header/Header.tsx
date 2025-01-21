1e100da8ee2fece37953641cf9ec1d252e3c66b2
Reniel George Mababa <mabsrencode@gmail.com>
2025-01-21 19:02:37 +0800

    update: document list component.

--- components/common/Header/Header.tsx
+++ components/common/Header/Header.tsx
@@ -8,20 +8,11 @@ const Header = ({ children, className }: HeaderProps) => {
     <div className={cn("header", className)}>
       <Link href={"/"} className="md:flex-1">
         <Image
-          src={"/assets/icons/logo.svg"}
-          alt="Logo"
-          width={120}
-          height={32}
-          className="hidden md:block"
-        />
-      </Link>
-      <Link href={"/"} className="md:flex-1">
-        <Image
           src={"/assets/icons/logo-icon.svg"}
           alt="Logo"
           width={32}
           height={32}
-          className="mr-2 md:hidden"
+          className="mr-2"
         />
       </Link>
       {children}
