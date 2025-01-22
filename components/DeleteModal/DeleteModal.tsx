bac4fb004d649592a665bd6f00c5d4a30dd449ae
Reniel George Mababa <mabsrencode@gmail.com>
2025-01-22 19:36:07 +0800

    update: cleanup.

--- components/DeleteModal/DeleteModal.tsx
+++ components/DeleteModal/DeleteModal.tsx
@@ -36,48 +36,12 @@ export const DeleteModal = ({ roomId }: DeleteModalProps) => {
   };
 
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger asChild>
-        <Button className="min-w-9 rounded-xl bg-transparent p-2 transition-all">
-          <Image
-            src="/assets/icons/delete.svg"
-            alt="delete"
-            width={20}
-            height={20}
-            className="mt-1"
-          />
-        </Button>
-      </DialogTrigger>
-      <DialogContent className="shad-dialog">
-        <DialogHeader>
-          <Image
-            src="/assets/icons/delete-modal.svg"
-            alt="delete"
-            width={48}
-            height={48}
-            className="mb-4"
-          />
-          <DialogTitle>Delete document</DialogTitle>
-          <DialogDescription>
-            Are you sure you want to delete this document? This action cannot be
-            undone.
-          </DialogDescription>
-        </DialogHeader>
-
-        <DialogFooter className="mt-5">
-          <DialogClose asChild className="w-full bg-dark-400 text-white">
-            Cancel
-          </DialogClose>
-
-          <Button
-            variant="destructive"
-            onClick={deleteDocumentHandler}
-            className="gradient-red w-full"
-          >
-            {loading ? "Deleting..." : "Delete"}
-          </Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+    <Button
+      variant="destructive"
+      onClick={deleteDocumentHandler}
+      className="gradient-red w-full"
+    >
+      {loading ? "Deleting..." : "Delete"}
+    </Button>
   );
 };
